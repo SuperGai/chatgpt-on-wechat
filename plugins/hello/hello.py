cd4efc67ad30ba59060edca5b95c28d438668981
Xu wenjuan <xuwenjuan7585@qq.com>
2023-06-19 22:07:55 +0800

    Update hello.py

--- plugins/hello/hello.py
+++ plugins/hello/hello.py
@@ -33,14 +33,14 @@ class Hello(Plugin):
         if e_context["context"].type == ContextType.JOIN_GROUP:
             e_context["context"].type = ContextType.TEXT
             msg: ChatMessage = e_context["context"]["msg"]
-            e_context["context"].content = f'请你随机使用一种风格说一句问候语来欢迎新用户"{msg.actual_user_nickname}"加入群聊。'
+            e_context["context"].content = f'请你随机使用一种风格说一句问候语来欢迎新用户"{msg.actual_user_nickname}"加入盖盖ChatGPT测试群聊。'
             e_context.action = EventAction.BREAK  # 事件结束，进入默认处理逻辑
             return
 
         if e_context["context"].type == ContextType.PATPAT:
             e_context["context"].type = ContextType.TEXT
             msg: ChatMessage = e_context["context"]["msg"]
-            e_context["context"].content = f"请你随机使用一种风格介绍你自己，并告诉用户输入#help可以查看帮助信息。"
+            e_context["context"].content = f"请你随机使用一种风格介绍你自己，并告诉用户输入#help可以查看帮助信息。并告诉用户有想免费体验可以联系WeChat:GAIGAI520"
             e_context.action = EventAction.BREAK  # 事件结束，进入默认处理逻辑
             return
 
